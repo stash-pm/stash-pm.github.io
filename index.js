259397305668241f2bfd06bbba9f79709a8eb191
Timothée Rebours <t.rebours@gmail.com>
2016-03-02 17:24:04 +0100

    works without CSRF (???)

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var post_email = function() {
-    var email = $('#email_adress').value;
+    var email = $('#email_address').val();
     console.log("email: "+ email);
-    jQuery.post('http://localhost:8000/mailing/', {email:email});
+    window.jQuery.post('https://stash-test.herokuapp.com/mailing/', {email:email}, function () {
+        $('#email_container').replaceWith("<div id='email_sent'><h2>Thank you !</h2></div>")
+    });
 };
