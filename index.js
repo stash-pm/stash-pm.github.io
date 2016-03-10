389d64c300cf892c23abfef3735d7c1b2789731a
arantes555 <arantes555@gmail.com>
2016-03-10 14:51:29 +0100

    Changing the adress in index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var post_email = function() {
     var email = $('#email_address').val();
     console.log("email: "+ email);
-    window.jQuery.post('https://stash-test.herokuapp.com/mailing/', {email:email}, function () {
+    window.jQuery.post('https://api-test.stash.pm/mailing/', {email:email}, function () {
         $('#email_container').replaceWith("<div id='email_sent'><h2>Thank you !</h2></div>")
     });
 };
